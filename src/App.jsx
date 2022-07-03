4e386cdf179302b4865eaf89b2024c97fc088890
waifonglee <waifonglee_@hotmail.com>
2022-07-04 00:08:22 +0800

    wait for 2seconds before loading the page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,27 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import History from "./components/History";
-import { MotionDiv } from "./components/helper";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [headerLoaded, setHeaderLoaded] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHeaderLoaded(true)
+    }, 2500)
+  })
+
   return (
     <VStack>
-      <Landing/>
-      <VStack w={['100%', '100%', '80%', '70%']} maxW='1200px' gap='5em' >
-        <About />
-        <Projects/>
-        <History/>
-        <Footer/>
-      </VStack>
+      <Landing />
+      {headerLoaded ?
+        <VStack w={['100%', '100%', '80%', '70%']} maxW='1200px' gap='5em' >
+          <About />
+          <Projects />
+          <History />
+          <Footer />
+        </VStack> : null}
     </VStack>
   );
 }
