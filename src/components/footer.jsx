a05db079a3ed3c5f4cde18ac7d2533c809c6e4c8
waifonglee <waifonglee_@hotmail.com>
2022-07-04 00:13:36 +0800

    move footer to left

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react"
 const Footer = () => {
     return (
         <Box w='100%' p='1em'>
-            <Text textAlign='center' > 2022 </Text>
+            <Text textAlign='left' > 2022 </Text>
         </Box>
     )
 }
