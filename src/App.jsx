36d6f447742a4a4b1f705fc4007ec0fc15725843
waifonglee <waifonglee_@hotmail.com>
2022-07-04 00:13:08 +0800

    remove commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setHeaderLoaded(true)
-    }, 2500)
+    }, 3000)
   })
 
   return (
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,4 @@
-import { Box, Image, Center } from "@chakra-ui/react"
+import { Box, Image, Center, Text } from "@chakra-ui/react"
 import { useAnimation } from 'framer-motion'
 import { MotionDiv, MotionSpan } from "./helper"
 
@@ -58,7 +58,6 @@ const Landing = () => {
                 </MotionDiv>
 
             </Center>
-
         </Box>
 
     )
@@ -106,151 +105,4 @@ const TextAnimation = (props) => {
 }
 
 
-/*
-const Header = () => {
-    const StarContainer1 = getAniContainer()
-    const StarContainer2 = getAniContainer()
-    const starTransition = {
-        duration: 4,
-        repeat: Infinity,
-        repeatType: "loop"
-    }
-    const ParallaxContainer = getAniContainer()
-    const parallaxControls = useAnimation()
-
-    const handleMouseMove = (e) => {
-        const offset = 100
-        const moveX = (e.clientX - window.innerWidth) / offset
-        const moveY = (e.clientY - window.innerHeight) / offset
-        parallaxControls.start({
-            x: moveX,
-            y: moveY
-        })
-    }
-
-    const title = 'WAI FONG LEE'
-    const subtitle = 'software engineer'
-
-    const headingAni = {
-        hidden: { opacity: 1 },
-        visible: {
-            opacity: 1,
-            transition: {
-                delay: 1,
-                staggerChildren: 0.15
-            }
-        }
-    }
-
-    const letterAni = {
-        hidden: { opacity: 0, y: 50 },
-        visible: {
-            opacity: 1,
-            y: 0
-        }
-    }
-
-    const AniText = chakra(motion.h1, {
-        shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === 'children',
-    })
-
-    const AniSpan = chakra(motion.span, {
-        shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === 'children',
-    })
-
-    const Test = getAniContainer()
-
-
-    const viewportControl = useAnimation()
-    const viewportVariant = {
-        offScreen: {
-            opacity: 0,
-            transition: {
-                ease: "easeOut",
-                duration: 0.5
-            }
-        },
-        onScreen: {
-            opacity: 1,
-            transition: {
-                ease: "easeIn",
-                duration: 0.5
-            }
-        }
-    }
-
-    const onVpEntry = () => {
-        viewportControl.start({
-            opacity: [0,0.5,1],
-            transition: {
-                ease: "easeIn",
-                duration: 2
-            }
-        })
-    }
-    
-    const onVpLeave = () => {
-        viewportControl.start({
-            opacity: 0,
-            transition: {
-                ease: "easeOut",
-                duration: 1
-            }
-        })
-    }
-
-
-    return (
-        <Box w='100%' h='100vh' bgGradient="linear(to-b, #020111 10%,#3a3a52 100%)" position='relative' onMouseMove={handleMouseMove} >
-            <Test
-                animate={{ opacity: [0, 1] }} transition={{ delay: 2, ease: "easeIn"}} position='absolute' h='100%' w='100%' >
-                <ParallaxContainer position='absolute' h='100%' w='100%' animate={parallaxControls}>
-                    <Image src='moon.svg' position='absolute' right='5vw' top='5vh' />
-                        <Image src='cloud.svg' position='absolute' right='7vw' top='9vh' />
-                </ParallaxContainer>
-                    <StarContainer1 h='100%' w='100%'
-                        animate={{
-                            opacity: [0, 0.25, 0.5, 0.75, 1, 0.75, 0.5, 0.25]
-                        }}
-                        transition={starTransition}
-                        position='absolute'
-                    >
-                        <Image src='stars1.png' h='100%' w='100%' />
-                    </StarContainer1>
-                    <StarContainer2 h='100%' w='100%'
-                        animate={{
-                            opacity: [1, 0.75, 0.5, 0.25, 0, 0.25, 0.5, 0.75]
-                        }}
-                        transition={starTransition}
-                        position='absolute'
-                    >
-                        <Image src='stars2.png' h='100%' w='100%' />
-                    </StarContainer2>
-
-                <Test position='absolute' h='100%' w='100%'  >   
-                    <motion.img src='lighthouse.svg' position='absolute' left='7vw' bottom='0vh' animate={viewportControl} onViewportLeave={onVpLeave} onViewportEnter={onVpEntry} />
-                    <Image src='cloud.svg' position='absolute' left='8vw' bottom='0vh' />
-                </Test>
-            </Test>
-            <AniText
-                variants={headingAni}
-                animate="visible"
-                initial="hidden"
-                position='absolute'
-                color='white'
-                h='100%' w='100%'
-                fontSize='50px'
-            >
-                {title.split("").map((c, ind) => {
-                    return (
-                        <AniSpan key={c + ind} variants={letterAni}>
-                            {c}
-                        </AniSpan>
-                    )
-                })}
-            </AniText>
-        </Box>
-    )
-}
-*/
 export default Landing
